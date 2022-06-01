c4bf363a3280ee578b61f23a78f85fdda3c49659
zeynab <zeynab.mvs@gmail.com>
2022-06-01 09:39:59 +0430

    overlay fade in/out workes

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+const btnHamburgur = document.querySelector('#jsbtnHamburgur')
+const header = document.querySelector('.header')
+const overlay =
+
+
+btnHamburgur.addEventListener('click', function(){ 
+    console.log('clicked')
+    
+    if(header.classList.contains('open')){ // Close hamburgur menu
+        header.classList.remove('open')
+    } else {
+        header.classList.add('open') // Open hamburgur menu
+    } 
+})
